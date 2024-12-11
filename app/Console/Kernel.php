29812ad14b915d8e68094594b19619d32597d707
unisexx <unisexx@gmail.com>
2024-12-11 21:24:34 +0700

    updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
+        \App\Console\Commands\DeleteOldProductViews::class,
         \App\Console\Commands\RunCrawlerTopCreator::class,
     ];
 
